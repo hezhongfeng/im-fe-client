c9b89cf120f86df6167dbea6320fde7a224aa71e
hezhongfeng <287645587@qq.com>
2020-03-23 10:49:25 +0800

    feat: add vant ui

--- src/main.js
+++ src/main.js
@@ -2,8 +2,12 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import Vant from 'vant';
+import 'vant/lib/index.css';
 
 Vue.config.productionTip = false;
+
+Vue.use(Vant);
 
 new Vue({
   router,
