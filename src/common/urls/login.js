8faa00209bc494d3bccba2be043de516b48fb7bd
hezhongfeng <287645587@qq.com>
2020-03-24 16:24:30 +0800

    feat: config urls

--- src/common/urls/login.js
+++ src/common/urls/login.js
@@ -1,13 +1,4 @@
 export default {
-  checkInfo: '/rest/member/check.jhtml', // 基础信息
-  defaultShop: '/rest/store/store.jhtml', // 店铺信息
-  captcha: '/rest/member/captcha.jhtml', // 校验码
-  validateCode: '/rest/member/sendValidateCode.jhtml', // 验证码
-  login: '/rest/member/login.jhtml', // 登录
-  bind: '/api/wechat/bind.jhtml', // 微信绑定
-  quickBind: '/api/wechat/quickbind.jhtml', // 微信快捷绑定
-  signup: '/rest/member/quickLogin.jhtml', // 注册
-  signupFromActivity: '/rest/member/registeByActivity.jhtml', // 通过活动注册
-  userInfo: '/rest/member/userInfo.jhtml', // 用户信息
-  password: '/rest/member/password.jhtml' // 密码
+  login: '/api/v1/login', // 登录
+  signup: '/api/v1/signup' // 注册
 };
