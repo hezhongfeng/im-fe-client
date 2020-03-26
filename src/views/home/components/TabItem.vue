cd31e66175e2da595d4460254f13b690e31c03a7
hezhongfeng <287645587@qq.com>
2020-03-26 14:54:37 +0800

    feat(tab): 添加首页切换组件

---
+++ src/views/home/components/TabItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="tab-item">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TabItem',
+  components: {},
+  props: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  watch: {},
+  created() {},
+  methods: {}
+};
+</script>
+
+<style lang="scss">
+.tab-item {
+  width: 100vw;
+  height: 100%;
+}
+</style>
---
+++ src/views/home/components/TabSwitch.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="tab-switch">
+    <div class="trans-zone" :style="styleObject">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'tab-switch',
+  components: {},
+  props: {
+    index: {
+      type: Number,
+      default() {
+        return 0;
+      }
+    }
+  },
+  data() {
+    return {
+      transitionName: 'forward'
+    };
+  },
+  computed: {
+    styleObject() {
+      return {
+        transform: `translateX(-${this.index}00vw)`
+      };
+    }
+  },
+  watch: {},
+  created() {},
+  methods: {}
+};
+</script>
+
+<style lang="scss">
+.tab-switch {
+  overflow: hidden;
+  height: calc(100vh - 50px);
+  .trans-zone {
+    display: flex;
+    transition-duration: 0.3s;
+  }
+}
+</style>
