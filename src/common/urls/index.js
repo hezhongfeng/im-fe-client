3624fcc6652f35f6b82dff3b3cb0f3874c7670fc
hezhongfeng <287645587@qq.com>
2020-03-31 15:12:40 +0800

    feat(urls): 单列restful

--- src/common/urls/index.js
+++ src/common/urls/index.js
@@ -1,6 +1,8 @@
 // 登录注册
 import login from './login';
+import restful from './restful';
 
 export default {
-  login
+  login,
+  restful
 };
---
+++ src/common/urls/restful.js
@@ -0,0 +1,4 @@
+export default {
+  sessions: '/api/v1/sessions', // 会话
+  groups: '/api/v1/groups' // 组
+};
