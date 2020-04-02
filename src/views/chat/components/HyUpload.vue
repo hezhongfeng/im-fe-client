5f999e36a26cca61a5ae46033f4d78b0f83e699b
hezhongfeng <287645587@qq.com>
2020-04-02 13:50:29 +0800

    feat(upload): 上传组件优化

--- src/views/chat/components/HyUpload.vue
+++ src/views/chat/components/HyUpload.vue
@@ -1,20 +1,18 @@
 <template>
   <div class="hy-upload">
-    <input ref="input" type="file" @change="changeHandler" :multiple="multiple" :accept="accept">
+    <input ref="input" type="file" @change="changeHandler" :multiple="multiple" :accept="accept" />
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
-import axios from 'axios';
+// import axios from 'axios';
 import store from '@/store';
-import throttle from 'lodash.throttle';
+// import throttle from 'lodash.throttle';
 
 export default {
   name: 'HyUpload',
   components: {},
   props: {
-    fileList: Array,
     limit: Number,
     multiple: {
       type: Boolean,
@@ -28,11 +26,7 @@ export default {
       type: Object,
       default() {
         return {
-          fileName: 'file',
-          data: {
-            type: '2'
-          },
-          target: this.host || '' + '/rest/file/uploads.jhtml'
+          target: '/api/v1/upload'
         };
       }
     }
@@ -40,24 +34,18 @@ export default {
   data() {
     return {};
   },
-  computed: {
-    ...mapGetters(['host', 'userInfo'])
-  },
-  watch: {},
-  created() {},
   methods: {
     click() {
       this.$refs.input.click();
     },
     changeHandler(e) {
-      const fileEle = e.currentTarget;
-      const files = fileEle.files;
+      const files = e.currentTarget.files;
       if (files) {
         this.uploadFiles(files);
       }
     },
     uploadFiles(files) {
-      if (this.limit && this.fileList.length + files.length > this.limit) {
+      if (this.limit && files.length > this.limit) {
         return;
       }
 
@@ -69,43 +57,18 @@ export default {
       if (postFiles.length === 0) {
         return;
       }
-
-      postFiles.forEach(rawFile => {
-        let timestamp = new Date().getTime();
-        this.addUploading(timestamp);
-        this.upload({ rawFile, timestamp });
-      });
+      this.upload(postFiles);
     },
-    upload({ rawFile, timestamp }) {
+    upload(files) {
       this.$refs.input.value = null;
-      let formData = new FormData();
-      formData.append(this.action.fileName, rawFile);
-      if (this.action.data) {
-        Object.keys(this.action.data).forEach(key => {
-          formData.append(key, this.action.data[key]);
-        });
+      const formData = new FormData();
+      for (const file of files) {
+        formData.append(file.name, file);
       }
-      let config = {
-        onUploadProgress: throttle(progressEvent => {
-          let progress = (progressEvent.loaded / progressEvent.total) * 100 || 0;
-          store.commit('CustomService/updateProgressByTimestamp', {
-            timestamp,
-            progress
-          });
-        }, 100),
-        headers: {
-          'Content-Type': 'multipart/form-data'
-        }
-      };
-      axios
-        .post(this.action.target, formData, config)
-        .then(res => {
-          if (res.data.resultCode === '100') {
-            store.commit('CustomService/clearMessageByTimestamp', {
-              timestamp
-            });
-            this.$emit('file-success', res.data);
-          }
+      this.$http
+        .post(this.action.target, formData)
+        .then(data => {
+          this.$emit('file-success', data);
         })
         .catch(error => {
           console.log(error);
