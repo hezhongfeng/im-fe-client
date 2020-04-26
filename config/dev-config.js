98c0835239d2989f742b9a5432a5b7544ea790b4
hezhongfeng <287645587@qq.com>
2020-04-26 16:50:40 +0800

    feat(dev): 调整开发数据

--- config/dev-config.js
+++ config/dev-config.js
@@ -3,7 +3,7 @@ module.exports = {
   // userInfo: {
   //   id: 2,
   //   nickname: 'hezf',
-  //   photo: null,
+  //   photo: '/public/images/head.png',
   //   sign: null,
   //   createdAt: '2020-03-27 16:27:56',
   //   updatedAt: '2020-03-27 16:27:56',
