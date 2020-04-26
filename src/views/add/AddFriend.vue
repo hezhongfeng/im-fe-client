4d26a712d53c85aec8fe2c05d2e61a44c3b52d10
hezhongfeng <287645587@qq.com>
2020-04-26 15:53:31 +0800

    feat: 添加搜索列表

--- src/views/add/AddFriend.vue
+++ src/views/add/AddFriend.vue
@@ -4,17 +4,26 @@
     <form action="/">
       <van-search v-model="searchValue" show-action placeholder="请输入搜索关键词" @search="onSearch" @cancel="onCancel" />
     </form>
+    <div class="f-list">
+      <friend v-for="user of users" :key="user.id" :friend="user" type="user"></friend>
+    </div>
   </div>
 </template>
 
 <script>
+import friend from './compenents/Friend';
+
 export default {
   name: 'add-friend',
-  components: {},
+  components: {
+    friend
+  },
   props: {},
   data() {
     return {
-      searchValue: ''
+      searchValue: '',
+      users: [],
+      groups: []
     };
   },
   computed: {},
@@ -27,7 +36,10 @@ export default {
         .post(this.$urls.add.search, {
           searchValue: this.searchValue
         })
-        .then(data => {})
+        .then(data => {
+          this.users = data.users;
+          this.groups = data.groups;
+        })
         .catch(error => {
           this.$toast(error.errorMessage);
         });
---
+++ src/views/add/compenents/Friend.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="friend">
+    <div class="name">{{name}}</div>
+    <div class="type">{{typeStr}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'friend',
+  components: {},
+  props: {
+    friend: Object,
+    type: String
+  },
+  data() {
+    return {};
+  },
+  computed: {
+    name() {
+      switch (this.type) {
+        case 'user':
+          return this.friend.username;
+
+        default:
+          break;
+      }
+      return '';
+    },
+    typeStr() {
+      return this.type === 'user' ? '会员' : '群组';
+    }
+  },
+  watch: {},
+  created() {},
+  methods: {}
+};
+</script>
+
+<style lang="scss">
+.friend {
+  display: flex;
+}
+</style>
