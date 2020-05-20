a6206e6296aa3f73e56b4b8969c1e8df4779ac0a
hezhongfeng <287645587@qq.com>
2020-05-20 15:17:15 +0800

    feat: 添加处理申请的接口

--- src/common/urls/add.js
+++ src/common/urls/add.js
@@ -1,4 +1,6 @@
 export default {
   search: '/api/v1/add/search', // 搜索ies
-  applies: '/api/v1/applies' // 申请列表
+  applies: '/api/v1/applies', // 申请列表
+  applyFriend: '/api/v1/add/friend', // 处理添加好友的请求
+  applyGroup: '/api/v1/add/group' // 处理入群申请的请求
 };
